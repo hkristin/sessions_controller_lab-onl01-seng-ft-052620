031bab2086e8def6bdd395d3dfe57207e136382a
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-13 19:59:14 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
     if !params[:name] || params[:name].empty?
       redirect_to(controller: 'sessions', action: 'new')
     else
-      session[:name] = params[:rname]
-      rredirect_to controller: 'application', action: 'hello'
+      session[:name] = params[:name]
+      redirect_to controller: 'application', action: 'hello'
   end
 end
 
